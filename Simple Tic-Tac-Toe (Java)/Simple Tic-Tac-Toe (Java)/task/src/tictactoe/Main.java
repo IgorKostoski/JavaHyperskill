deb0a0527d0ca32e6d92b482d3107eac5f8676df
Igor Kostoski <igorkostoski@ymail.com>
2024-03-07 18:11:10 +0100

    commit

--- Simple Tic-Tac-Toe (Java)/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -3,5 +3,8 @@ package tictactoe;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        System.out.print("X" +  "O" + "X");
+        System.out.print("O" + "X" + "O");
+        System.out.print("X" + "O" + "X");
     }
 }
